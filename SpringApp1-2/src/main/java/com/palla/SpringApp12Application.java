07cf983094145d7ee1f92eb72884fa7503538931
Narasimha palla <34704903+narasimhapalla@users.noreply.github.com>
2022-03-09 19:51:08 +0530

    new app

---
+++ SpringApp1-2/src/main/java/com/palla/SpringApp12Application.java
@@ -0,0 +1,13 @@
+package com.palla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApp12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApp12Application.class, args);
+	}
+
+}
