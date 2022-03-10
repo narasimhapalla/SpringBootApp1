3a6fe01cbe65326e934cc60de19e8afe602dcbaa
Narasimha palla <34704903+narasimhapalla@users.noreply.github.com>
2022-03-10 11:02:45 +0530

    JPS rep

--- SpringApp1-2/src/main/java/com/palla/CustomerRep.java
+++ SpringApp1-2/src/main/java/com/palla/CustomerRep.java
@@ -1,7 +1,8 @@
 package com.palla;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CustomerRep extends CrudRepository<Customers, Integer> {
-
+//public interface CustomerRep extends CrudRepository<Customers, Integer> {
+public interface CustomerRep extends JpaRepository<Customers, Integer> {
 }
--- SpringApp1-2/src/main/java/com/palla/FormController.java
+++ SpringApp1-2/src/main/java/com/palla/FormController.java
@@ -1,13 +1,19 @@
 package com.palla;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class FormController {
@@ -32,12 +38,25 @@ public class FormController {
 	}
 	
 	
-	@PostMapping("getDetails")
+	@PostMapping("/getDetails")
 	public ModelAndView getDetails(@RequestParam("cid") int cid) {
 		ModelAndView model = new ModelAndView("Retrieve");
 		Customers customers = rep.findById(cid).orElse(null); 
 		model.addObject(customers);
 		return model;
 	}
+	
+	@RequestMapping("/customers")
+	@ResponseBody
+	public List<Customers> getCustomers() {
+		
+		return rep.findAll();
+	}
 
+	@RequestMapping("/customers/{cid}")
+	@ResponseBody
+	public Optional<Customers> getCustomersById(@PathVariable("cid")int cid) {
+		
+		return rep.findById(cid);
+	}
 }
