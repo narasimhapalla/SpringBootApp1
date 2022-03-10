0f5c477065c5d05583973103381efe7921345797
Narasimha palla <34704903+narasimhapalla@users.noreply.github.com>
2022-03-10 12:30:32 +0530

    new changes

--- SpringApp1-2/src/main/java/com/palla/FormController.java
+++ SpringApp1-2/src/main/java/com/palla/FormController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -62,6 +63,11 @@ public class FormController {
 	}
 	@PostMapping("/customers")
 	public Customers postCustomer(@RequestBody Customers customer) {
+		rep.save(customer);
+		return customer;
+	}
+	@PutMapping(path = "customers", consumes = {"application/json"})
+	public Customers putCustomer(@RequestBody Customers customer)  {
 		rep.save(customer);
 		return customer;
 	}
