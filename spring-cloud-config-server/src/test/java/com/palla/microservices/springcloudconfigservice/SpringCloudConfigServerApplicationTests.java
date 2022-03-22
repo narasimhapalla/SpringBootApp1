c2a13a403fef0e88bb82c5ac30c73d979c75fd1b
Narasimha palla <34704903+narasimhapalla@users.noreply.github.com>
2022-03-22 17:07:10 +0530

    config server

---
+++ spring-cloud-config-server/src/test/java/com/palla/microservices/springcloudconfigservice/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palla.microservices.springcloudconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
