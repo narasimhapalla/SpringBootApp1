c20892c09d8978773a4082986f9e5246c5a4db15
Narasimha palla <34704903+narasimhapalla@users.noreply.github.com>
2022-03-28 12:15:53 +0530

    distributing tracing server zipkin configuration.

--- currency-exchange-service/src/main/java/com/palla/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/palla/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.palla.microservices.currencyexchangeservice.controller;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,7 @@ import com.palla.microservices.currencyexchangeservice.repository.CurrencyExchan
 @RestController
 public class CurrencyExchangeController {
 	
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 	@Autowired
 	private Environment environment;
 	@Autowired
@@ -23,6 +26,7 @@ public class CurrencyExchangeController {
 	public CurrencyExchange retrieveExchangeValues(
 			@PathVariable String from,
 			@PathVariable String to) {
+		logger.info("retrieveExchangeValues called with {} to {}",from,to);
 		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 		if(currencyExchange == null) {
 			throw new RuntimeException("Unable to find data for -> "+from+" to "+to);
